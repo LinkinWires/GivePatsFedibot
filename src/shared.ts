9caf46d5e9741ae82741ec5a7f9ffe3ec029118c
linkinwires <linkinwires@gmail.com>
2025-07-17 07:42:33 +0300

    Shared functions Functions that could be used in any file, such as getting MIME of file and generating pat-pat gif

---
+++ src/shared.ts
@@ -0,0 +1,25 @@
+import petPetGif from "@someaspy/pet-pet-gif";
+import { $ } from "bun";
+
+export function getMime(file: string | Buffer): string {
+    if (typeof file === 'string') {
+        const bunFile = Bun.file(file);
+        if (!bunFile.exists()) throw Error('This file does not exist');
+        else return bunFile.type;
+    } else {
+        const blob = new Blob([file]);
+        return blob.type;
+    }
+}
+
+export async function pat(filename: string): Promise<Buffer> {
+    const mime = getMime(filename);
+    const format = mime.replace('image/', '');
+    let finalFilename = '';
+    if (mime !== 'image/png') {
+        const newFilename = `${filename.replace('tmp', 'png')}`;
+        await $`ffmpeg -y -i ${filename} ${newFilename}`;
+        finalFilename = newFilename;
+    } else finalFilename = filename;
+    return await petPetGif(finalFilename);
+}
