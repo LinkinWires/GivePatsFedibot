91156e90e3bdbc57a8c06e593d3e7b5098766d8f
linkinwires <linkinwires@gmail.com>
2025-07-20 22:46:30 +0300

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,19 @@ if (!process.env.SOFTWARE) throw Error('No software provided');
 let instance = null;
 switch (process.env.SOFTWARE) {
     case 'misskey':
-        instance = new MisskeyDriver(process.env.INSTANCE_URL, process.env.ACCESS_TOKEN);
+        instance = new MisskeyDriver(
+            process.env.INSTANCE_URL, 
+            process.env.ACCESS_TOKEN, 
+            process.env.MISSKEY_MODE && process.env.MISSKEY_MODE === 'gif' || process.env.MISSKEY_MODE === 'emoji' ? process.env.MISSKEY_MODE : 'gif'
+        );
         break;
     case 'mastodon':
         if (!process.env.BOT_USERNAME) throw Error('Mastodon driver required BOT_USERNAME env variable defined (example: bot, without the @ and the instance domain name)')
-        instance = new MastodonDriver(process.env.INSTANCE_URL, process.env.ACCESS_TOKEN, process.env.BOT_USERNAME);
+        instance = new MastodonDriver(
+            process.env.INSTANCE_URL, 
+            process.env.ACCESS_TOKEN, 
+            process.env.BOT_USERNAME
+        );
         break;
 }
 if (!instance) throw Error('Unsupported software')
